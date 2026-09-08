53b2eb71cedb3d6b4de82a077e2b2728e69aef69
ibraheemriaz7 <ibraheemriaz321@gmail.com>
2026-09-08 22:30:53 +0500

    Add files via upload

---
+++ lab_num/Q4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float temperature;
+
+    printf("Enter temperature: ");
+    scanf("%f", &temperature);
+
+    printf("\nTemperature: %.2f C\n", temperature);
+
+    return 0;
+}
